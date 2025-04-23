cf6d03575d21a2bf3dd91d48a10bb707fb197a74
Nikhil Chauhan <nikhilchauha007@gmail.com>
2025-04-23 12:51:22 +0530

    Add files via upload

---
+++ cps.js
@@ -0,0 +1,8 @@
+    
+   
+function areaofsquare(){
+    let l=document.getElementById("input1").innerHTML;
+    let b=document.getElementById("input2").innerHTML;
+    let area=l*b;
+    console.log(area);
+}
